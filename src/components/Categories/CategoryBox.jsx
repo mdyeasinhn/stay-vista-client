e5e3c6b16065cbb7e14eb4231ef4bdcce5fc715c
Md Yeasin <mdyeasinhn@gmail.com>
2024-08-28 19:03:16 +0600

    initial projects setup

---
+++ src/components/Categories/CategoryBox.jsx
@@ -0,0 +1,41 @@
+import PropTypes from 'prop-types'
+import queryString from 'query-string';
+import { useNavigate } from 'react-router-dom';
+
+const CategoryBox = ({ label, icon: Icon }) => {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    let currentQuery = { category: label }
+    const url = queryString.stringifyUrl({
+      url: '/',
+      query: currentQuery,
+    });
+    navigate(url)
+  }
+
+  return (
+    <div
+      onClick={handleClick}
+      className={`flex 
+  flex-col 
+  items-center 
+  justify-center 
+  gap-2
+  p-3
+  border-b-2
+  hover:text-neutral-800
+  transition
+  cursor-pointer`}
+    >
+      <Icon size={26} />
+      <div className='text-sm font-medium'>{label}</div>
+    </div>
+  )
+}
+
+CategoryBox.propTypes = {
+  label: PropTypes.string,
+  icon: PropTypes.elementType,
+}
+
+export default CategoryBox
