d284a83f93825d48cb3e40d7635cfc759fd4c47d
Md Yeasin <mdyeasinhn@gmail.com>
2024-08-28 19:48:52 +0600

    category tabs functionality completed

--- src/components/Categories/CategoryBox.jsx
+++ src/components/Categories/CategoryBox.jsx
@@ -1,15 +1,22 @@
 import PropTypes from 'prop-types'
 import queryString from 'query-string';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 
 const CategoryBox = ({ label, icon: Icon }) => {
   const navigate = useNavigate();
+  const [params, setParams] = useSearchParams();
+  const category = params.get('category');
+
   const handleClick = () => {
+    // 1. create query string
     let currentQuery = { category: label }
     const url = queryString.stringifyUrl({
       url: '/',
       query: currentQuery,
     });
+    // url output --> /?category=label
+
+    //2.set query string in uri
     navigate(url)
   }
 
@@ -17,15 +24,15 @@ const CategoryBox = ({ label, icon: Icon }) => {
     <div
       onClick={handleClick}
       className={`flex 
-  flex-col 
-  items-center 
-  justify-center 
-  gap-2
-  p-3
-  border-b-2
-  hover:text-neutral-800
-  transition
-  cursor-pointer`}
+                  flex-col 
+                  items-center 
+                  justify-center 
+                  gap-2
+                  p-3
+                  border-b-2
+                  hover:text-neutral-800
+                  transition
+                  cursor-pointer  ${category === label && 'border-b-neutral-800 text-neutral-800'}`}
     >
       <Icon size={26} />
       <div className='text-sm font-medium'>{label}</div>
