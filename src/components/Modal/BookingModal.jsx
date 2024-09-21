4f9c8fdb304dfe0b3e09733b1413cb9ef93d4611
Md Yeasin <mdyeasinhn@gmail.com>
2024-09-22 00:36:58 +0600

    stripe checkout form completed

--- src/components/Modal/BookingModal.jsx
+++ src/components/Modal/BookingModal.jsx
@@ -10,6 +10,7 @@ import { format } from 'date-fns'
 import { Fragment } from 'react'
 import {loadStripe} from '@stripe/stripe-js';
 import { Elements } from "@stripe/react-stripe-js";
+import CheckoutForm from '../Form/CheckoutForm';
 const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY);
 
 const BookingModal = ({ closeModal, isOpen, bookingInfo }) => {
@@ -77,26 +78,10 @@ const BookingModal = ({ closeModal, isOpen, bookingInfo }) => {
                                 <hr className='mt-8 ' />
                                 <Elements stripe={stripePromise}>
                                     {/* checkout form */}
+                                    <CheckoutForm closeModal={closeModal} bookingInfo={bookingInfo}/>
                                 </Elements>
 
-                                <div className='flex mt-2 justify-around'>
-                                    <button
-                                        type='button'
-                                        className='inline-flex justify-center rounded-md border border-transparent bg-green-100 px-4 py-2 text-sm font-medium text-green-900 hover:bg-green-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-green-500 focus-visible:ring-offset-2'
-
-                                    >
-                                        Book
-                                    </button>
-                                    <button
-
-                                        type='button'
-                                        className='inline-flex justify-center rounded-md border border-transparent bg-red-100 px-4 py-2 text-sm font-medium text-red-900 hover:bg-red-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-red-500 focus-visible:ring-offset-2'
-                                        onClick={closeModal}
-                                    >
-                                        No
-                                    </button>
-
-                                </div>
+                              
                             </DialogPanel>
                         </TransitionChild>
                     </div>
