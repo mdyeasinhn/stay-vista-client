8e8635f6536ca87a75e06383abc7bf9c9405e689
Md Yeasin <mdyeasinhn@gmail.com>
2024-09-22 23:24:43 +0600

    payment succeeded completed

--- src/components/Form/CheckOutForm.jsx
+++ src/components/Form/CheckOutForm.jsx
@@ -1,17 +1,38 @@
 
 
-import {CardElement,  useElements, useStripe} from '@stripe/react-stripe-js';
-
+import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
+import PropTypes from 'prop-types'
+import { ImSpinner9 } from 'react-icons/im'
 import './CheckOutForm.css';
-
-const CheckoutForm = ({closeModal, bookingInfo}) => {
+import { useEffect, useState } from 'react';
+import useAxisoSecure from '../../hooks/useAxiosSecure'
+import useAuth from '../../hooks/useAuth';
+const CheckoutForm = ({ closeModal, bookingInfo }) => {
+  const axiosSecure = useAxisoSecure();
+  const { user } = useAuth()
   const stripe = useStripe();
   const elements = useElements();
+  const [clientSecret, setClientSecret] = useState();
+  const [cardError, setCardError] = useState('')
+  const [processing, setProcessing] = useState(false)
+  useEffect(() => {
+    // Fetch client secret 
+    if (bookingInfo?.price && bookingInfo?.price > 1) {
+      getClientSecret({ price: bookingInfo.price })
+    }
+  }, [bookingInfo.price]);
+
+  // get client secret 
+  const getClientSecret = async price => {
+    const { data } = await axiosSecure.post('/create-payment-intent', price)
+    console.log("client secret for server", data);
+    setClientSecret(data.clientSecret)
+  }
 
   const handleSubmit = async (event) => {
     // Block native form submission.
     event.preventDefault();
-
+    setProcessing(true)
     if (!stripe || !elements) {
       // Stripe.js has not loaded yet. Make sure to disable
       // form submission until Stripe.js has loaded.
@@ -28,58 +49,102 @@ const CheckoutForm = ({closeModal, bookingInfo}) => {
     }
 
     // Use your card Element with other Stripe.js APIs
-    const {error, paymentMethod} = await stripe.createPaymentMethod({
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
       type: 'card',
       card,
     });
 
     if (error) {
       console.log('[error]', error);
+      setCardError(error.message);
+      setProcessing(false)
+      return
     } else {
       console.log('[PaymentMethod]', paymentMethod);
+      setCardError('')
     }
+    // confirm payment 
+    const { error: confirmError, paymentIntent } = await stripe.confirmCardPayment(clientSecret, {
+      payment_method: {
+        card: card,
+        billing_details: {
+          email: user?.email,
+          name: user?.displayName
+        }
+      }
+    })
+    if (confirmError) {
+      console.log(confirmError);
+      setCardError(confirmError.message);
+      setProcessing(false)
+      return
+    }
+
+    if (paymentIntent.status === 'succeeded') {
+      console.log(paymentIntent)
+      // 1. Create payment info object
+      const paymentInfo = {
+        ...bookingInfo,
+        roomId: bookingInfo._id,
+        transactionId: paymentIntent.id,
+        date: new Date(),
+      }
+      console.log(paymentInfo);
+    }
+    setProcessing(false)
+
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <CardElement
-        options={{
-          style: {
-            base: {
-              fontSize: '16px',
-              color: '#424770',
-              '::placeholder': {
-                color: '#aab7c4',
+    <>
+      <form onSubmit={handleSubmit}>
+        <CardElement
+          options={{
+            style: {
+              base: {
+                fontSize: '16px',
+                color: '#424770',
+                '::placeholder': {
+                  color: '#aab7c4',
+                },
+              },
+              invalid: {
+                color: '#9e2146',
               },
             },
-            invalid: {
-              color: '#9e2146',
-            },
-          },
-        }}
-      />
-    
-      <div className='flex mt-2 justify-around'>
-                                    <button
-                                    disabled={!stripe}
-                                        type='submit'
-                                        className='inline-flex justify-center rounded-md border border-transparent bg-green-100 px-4 py-2 text-sm font-medium text-green-900 hover:bg-green-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-green-500 focus-visible:ring-offset-2'
+          }}
+        />
 
-                                    >
-                                        Pay ${bookingInfo?.price}
-                                    </button>
-                                    <button
+        <div className='flex mt-2 justify-around'>
+          <button
+            disabled={!stripe || !clientSecret || processing}
+            type='submit'
+            className='inline-flex justify-center rounded-md border border-transparent bg-green-100 px-4 py-2 text-sm font-medium text-green-900 hover:bg-green-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-green-500 focus-visible:ring-offset-2'
 
-                                        type='button'
-                                        className='inline-flex justify-center rounded-md border border-transparent bg-red-100 px-4 py-2 text-sm font-medium text-red-900 hover:bg-red-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-red-500 focus-visible:ring-offset-2'
-                                        onClick={closeModal}
-                                    >
-                                        Cancel
-                                    </button>
+          >
+            {processing ? <ImSpinner9 className='animate-spin m-auto' size={24} /> : (`Pay ${bookingInfo?.price}`)}
 
-                                </div>
-    </form>
+          </button>
+          <button
+
+            type='button'
+            className='inline-flex justify-center rounded-md border border-transparent bg-red-100 px-4 py-2 text-sm font-medium text-red-900 hover:bg-red-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-red-500 focus-visible:ring-offset-2'
+            onClick={closeModal}
+          >
+            Cancel
+          </button>
+
+        </div>
+      </form>
+      {
+        cardError && <p className='text-red-600 ml-8'>{cardError}</p>
+      }
+    </>
   );
 };
-
+CheckoutForm.propTypes = {
+  bookingInfo: PropTypes.object,
+  closeModal: PropTypes.func,
+  refetch: PropTypes.func,
+}
 export default CheckoutForm
